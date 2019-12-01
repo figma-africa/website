91f7478b2df2e777b1bd2174a82a748eaa95d6da
Friday <faradayyg@gmail.com>
2019-12-01 05:59:40 +0100

    footer section and  section complete

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,10 +32,10 @@
 
         <section class="gallery">
             <div class="first-grid">
-                <div>
+                <div class="col-xs-6">
                     <img src="/images/home-image-1.jpg" alt="Figma africa gallery image" />
                 </div>
-                <div>
+                <div class="col-xs-6">
                     <img src="/images/home-image-2.jpg" alt="Figma africa gallery image" />
                 </div>
                 <div>
@@ -158,10 +158,89 @@
                         
                     </div>
             </section>
-
-            <section class="communities">
-                <h2>Our Communities</h2>
-            </section>
         </div>
+        <section class="communities">
+            <span>CITIES AND COUNTRIES</span>
+            <h2>Our Communities</h2>
+
+            <p>
+                    While we position ourselves to be the digital hub for everything design in Africa, 
+                    we exist in some cities in several countries across Africa which are 
+                    listed below. We also host physical events and activities which brings our members together in their individual cities.
+            </p>
+
+            <div class="flags-section">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <img src="/images/flags/Ghana.svg" alt=""> Accra, Ghana
+                    </div>
+                    <div class="col-sm-3">
+                        <img src="/images/flags/Nigeria.svg" alt=""> Osun, Nigeria
+                    </div>
+
+                    <div class="col-sm-3">
+                        <img src="/images/flags/Rwanda.svg" alt=""> Kigali, Rwanda
+                    </div>
+                    <div class="col-sm-3">
+                        <img src="/images/flags/Nigeria.svg" alt=""> Uyo, Nigeria
+                    </div>
+                    <div class="col-sm-3">
+                        <img src="/images/flags/Nigeria.svg" alt=""> Lagos, Nigeria
+                    </div>
+                    <div class="col-sm-3">
+                        <img src="/images/flags/Nigeria.svg" alt=""> Niger, Nigeria
+                    </div>
+                    <div class="col-sm-3">
+                        <img src="/images/flags/Kenya.svg" alt=""> Nairobi, Kenya
+                    </div>
+                    <div class="col-sm-3">
+                        <img src="/images/flags/Nigeria.svg" alt=""> Abuja, Nigeria
+                    </div>
+                    <div class="col-sm-3">
+                        <img src="/images/flags/Morocco.svg" alt=""> Casablanca, Morocco
+                    </div>
+                    <div class="col-sm-3">
+                        <img src="/images/flags/Nigeria.svg" alt=""> Kwara, Nigeria
+                    </div>
+                    <div class="col-sm-3">
+                        <img src="/images/flags/Nigeria.svg" alt=""> Umuahia, Nigeria
+                    </div>
+                    <div class="col-sm-3">
+                        <img src="/images/flags/Nigeria.svg" alt=""> Enugu, Nigeria
+                    </div>
+                    <div class="col-sm-3">
+                        <img src="/images/flags/Benin.svg" alt=""> Cotonou, Benin Republic
+                    </div>
+                    <div class="col-sm-3">
+                        <img src="/images/flags/Nigeria.svg" alt=""> Kaduna, Nigeria
+                    </div>
+                    <div class="col-sm-3">
+                        <img src="/images/flags/Congo.svg" alt=""> Kinshasa, Congo
+                    </div>
+                </div>
+            </div>
+            <a class="start-figma-group" href="https://docs.google.com/forms/d/e/1FAIpQLSdLQB6pyVBImwLBX-7FqM7GMEIjcIn2JM8u2Vi45wXyEXAzkg/viewform">
+                Start a figma group in your city
+            </a>
+        </section>
+        <section class="join-us">
+
+        </section>
+        <footer>
+            <ul class="nav nav-pills m-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><span class="fab fa-twitter"></span> Twitter</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><span class="fab fa-dribbble"></span> Dribbble</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><span class="fab fa-instagram"></span> Instagram</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><span class="fab fa-medium-m"></span> Medium</a>
+                </li>
+            </ul>
+        </footer>
     </body>
 </html>
