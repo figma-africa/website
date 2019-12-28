cda1a55cb5b24236d0f9fc3fd36b164ee12bfc3d
Friday <faradayyg@gmail.com>
2019-12-28 06:05:37 +0100

    dynamically display description of design challenge, show default when missing

--- resources/views/partials/moreEvents.blade.php
+++ resources/views/partials/moreEvents.blade.php
@@ -25,9 +25,13 @@
                     <h3>{{$designChallenge->title}}</h3>
                 </div>
                     <p>
-                            Our design challenge is held on a weekly basis and it is designed to enable people explore design concepts in a manner which pushes our creativity, challenges our exposure while offering competition in a positive approach.
+                    @if(strlen($designChallenge->description) > 10)
+                            {{$designChallenge->description}}
+                    @else
+                    Our design challenge is held on a weekly basis and it is designed to enable people explore design concepts in a manner which pushes our creativity, challenges our exposure while offering competition in a positive approach.
                             <br />&nbsp; <br />
                             Submissions are voted and the winning entry gets a prize as well as promotion on all of our platforms. 
+                            @endif
                     </p>
                     <a href="{{route('challenges')}}" class="view-entries" >View all entries &raquo;</a>
             </div>
