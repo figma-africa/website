0e62bd5b3e2949b63a6b6d994f86b938a9be206f
Friday <faradayyg@gmail.com>
2019-12-28 05:57:52 +0100

    remove default from descriptio

--- database/migrations/2019_12_28_030431_modify_design_challenge_table.php
+++ database/migrations/2019_12_28_030431_modify_design_challenge_table.php
@@ -16,16 +16,7 @@ class ModifyDesignChallengeTable extends Migration
         Schema::table('design_challenges', function (Blueprint $table) {
             $table->string('challenge_image_link')
                 ->default('/images/design-challenge.jpg');
-                $table->longText('description')->default(
-                    "Our design 
-                    challenge is held on a weekly basis and it is designed to enable 
-                    people 
-                    explore design concepts in a manner which pushes our creativity, 
-                    challenges our exposure while offering competition in a 
-                    positive approach.
-                    Submissions are voted and the winning entry gets a prize as well 
-                    as promotion on all of our platforms."
-                );
+                $table->longText('description');
         });
     }
 
