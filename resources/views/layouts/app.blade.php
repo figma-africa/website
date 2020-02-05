d4c07ce6bd689bc25649f28ea0f29830f49c5e10
Anthony Nwaizuzu <tonychuks7@gmail.com>
2020-02-05 19:58:42 +0100

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="Figma Africa is a community of web designers and other creatives, our events and activities are designed to enhance learning and understanding of design while bringing together members in the community into a physical or virtual space for them to connect and network.">
         <link rel="icon" href="/images/logo.png" type="image/png"/>
+        <meta http-equiv="Cache-control" content="public">
 
         <title>Figma Africa Website</title>
 
