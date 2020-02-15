4ace83116e87108c5e107de1427c31ba8671a683
FridayG <faradayyg@gmail.com>
2020-02-15 17:07:51 +0100

    added og:image to meta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,9 +3,14 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="Figma Africa is a community of web designers and other creatives, our events and activities are designed to enhance learning and understanding of design while bringing together members in the community into a physical or virtual space for them to connect and network.">
+        <meta name="description" content="Figma Africa is a community of web designers and other creatives, our events and activities are designed to enhance learning and understanding of design while bringing together members in the community into a physical or virtual space for them to connect and network." />
         <link rel="icon" href="/images/logo.png" type="image/png"/>
-        <meta http-equiv="Cache-control" content="public">
+        <meta http-equiv="Cache-control" content="public" />
+        <meta property="og:image" content="/images/ogimage.jpeg"/>
+        <meta property="og:title" content="Figmaafrica Design Community"/>
+        <meta property="og:description" content="Figma Africa is a community of web designers and other creatives, our events and activities are designed to enhance learning and understanding of design while bringing together members in the community into a physical or virtual space for them to connect and network." />
+        <meta property="og:url" content="https://figmaafrica.com"/>
+        <meta property="og:site_name" content="Figmaafrica.com"/>
 
         <title>Figma Africa Website</title>
 
